04591280ce0832237b7ed3a83ec62d58d16b3bf0
jshadbolt <146521761+jshadbolt@users.noreply.github.com>
2024-01-23 22:39:55 +1300

    add reliable chained calculation method

--- main.js
+++ main.js
@@ -1,109 +1,102 @@
 const numBtns = Array.from(document.querySelectorAll('.number'))
 const opBtns = Array.from(document.querySelectorAll('.operator'))
 const funcBtns = Array.from(document.querySelectorAll('.function'))
+
+let equalBtn = document.getElementById('equal')
+let clearBtn = document.getElementById('clear')
 let display = document.getElementById('display')
 
 let numArr = []
-let operator = []
+let operator = '+'
 let displayValue = ''
-let currVal = ''
+let currentNum = ''
+let operatorSelected = true
 
-function calc() {
-    let num1 = +numArr.pop()
-    let num2 = +numArr.pop()
-    let symbol = operator.pop()
-    let result;
-    switch (symbol) {
-        case '+':
-            result = num1 + num2
-            break;
-        case '-':
-            result = num1 - num2
-            break;
-        case '*':
-            result = num1 * num2
-            break;
-        case '/':
-            result = num1 / num2
-            break;
+
+function init() {
+}
+
+let calcFuncs = {
+    add : function(a, b) {
+        return a + b
+    },
+    subtract : function(a, b) {
+        return a - b
+    },
+    multiply : function(a, b) {
+        return a * b
+    },
+    divide : function(a, b) {
+        return a / b
+    },
+}
+
+function operate(operator) {
+    if (numArr.length > 1) {
+        let num1 = +numArr.shift()
+        let num2 = +numArr.shift()
+        switch (operator) {
+                case '+':
+                    numArr.unshift(calcFuncs.add(num1, num2))
+                    break;
+                case '-':
+                    numArr.unshift(calcFuncs.subtract(num1, num2))
+                    break;
+                case '*':
+                    numArr.unshift(calcFuncs.multiply(num1, num2))
+                    break;
+                case '/':
+                    numArr.unshift(calcFuncs.divide(num1, num2))
+                    break;
+        }
+        console.log(numArr)
+        display.innerHTML = numArr[0]
+    } else {
+        console.log('not long enough')
     }
-    numArr.push(result)
-    showDisplayValue(numArr[0])
 }
 
-function updateDisplayValue(value) {
-    displayValue += value
+function createNewNum(number) {
+    if (number.length < 1) {
+        console.log('wrong')
+        return
+    } else {
+    numArr.push(number)
+    // console.log(numArr)
+    }
 }
 
-function resetDisplayValue(value) {
-    displayValue = value
-}
-
-function showDisplayValue(value) {
-    display.innerHTML = value
-}
 
 numBtns.forEach(btn => {
-    let value = btn.value
     btn.addEventListener('click', () => {
-        updateDisplayValue(value)
-        showDisplayValue(displayValue)
+        // if (operatorSelected) {
+        value = btn.value
+        currentNum += value
+        console.log(currentNum)
+        // }
     })
 })
 
 opBtns.forEach(btn => {
-    let opSymbol = btn.value
     btn.addEventListener('click', () => {
-        updateOperator(opSymbol)
-        updateCurrValue(displayValue)
-        createNewNumber()
-        console.log(numArr)
+        operatorSelected = true
+        createNewNum(currentNum)
+        resetCurrentNum()
+        operate(operator)
+        operator = btn.value
         console.log(operator)
-        if (numArr.length === 2) {
-            calc()
-            
-        }
     })
 })
 
-// funcBtns.forEach(btn => {
-//     let func = btn.value
-//     btn.addEventListener('click', () => {
-//         funcObj[func]()
-//     })
-// })
+equalBtn.addEventListener('click', () => {
+    if (operatorSelected) {
+    createNewNum(currentNum)
+    resetCurrentNum()
+    operate(operator)
+    operatorSelected = false
+    }
+})
 
-function createNewNumber() {
-    numArr.unshift(currVal)
-    currVal = ''
-    resetDisplayValue(currVal)
+function resetCurrentNum() {
+    currentNum = ''
 }
-
-function updateCurrValue(value) {
-    currVal = value
-}
-
-function updateOperator(symbol) {
-    operator.unshift(symbol)
-}
-
-let funcObj = {
-    'equal': function() {
-
-    },
-    'del': function() {
-        
-    },
-    'clear': function() {
-        numArr = []
-    },
-}
-
-console.log(operator)
-
-
-
-
-
-
-//if array has two numbers, operate
